c708ef353cb5c52a797e04bad9e4c866b6044711
Franciscode10 <vallesfrancisconogues@gmail.com>
2023-08-21 19:08:24 -0300

    add sections of the course, now I´m going to watch the video course

---
+++ loops/for_loop.py
@@ -0,0 +1,20 @@
+#First loop in Python
+for i in [0,1,2,3,4,5]:
+    print(i)
+
+print("\nThe following code does the same as the code above")
+for i in range(6):
+    print(i)
+
+
+print("\nNames in a list")
+list = ["Francisco","Messi", "Jorge", "Lucía"]
+for i in list:
+    print(i)
+
+print("\nLoop through each character in a string")
+name = "Fran"
+
+for c in name:
+    print(c)
+
